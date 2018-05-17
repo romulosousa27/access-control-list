fb5ec89cdebdabe0b6c439deb74a8a38130e26ea
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-16 22:56:02 -0300

    relacionamento Noticias com User

--- acl/app/Noticia.php
+++ acl/app/Noticia.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Noticia extends Model
-{
+class Noticia extends Model{
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 
 }
