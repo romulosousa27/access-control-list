45ca62be6630e40b136e0dff087aec3891b02f9c
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-25 00:01:12 -0300

    link editar

--- acl/resources/views/home.blade.php
+++ acl/resources/views/home.blade.php
@@ -6,6 +6,7 @@
         <h1>{{$n->title}}</h1>
         <p>{{$n->description}}</p>
         <b>Autor: {{$n->user->name}}</b>
+        <a href="{{url("/noticia/$n->id/update")}}">editar</a>
     @empty
         <p class="bg-danger">Nenhuma Noticia Vinculada</p>
     @endforelse
