06f3f87ce8a010a9148b353c4d86f88ae04ab76a
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-25 00:00:27 -0300

    update noticia auth

--- acl/app/Http/Controllers/HomeController.php
+++ acl/app/Http/Controllers/HomeController.php
@@ -5,28 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Noticia;
 use App\User;
+use Gate;
 
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+class HomeController extends Controller{
+    
+    public function __construct(){
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Noticia $noticia){
         // retornando todas a noticias.
         $noticias = $noticia->all();
         
         return view('home', compact('noticias'));  
     }
+
+    public function update($noticiaID){
+        //retornando a noticia espeficica
+        $noticia = Noticia::find($noticiaID);
+
+        //$this->authorize('noticia-update', $noticia);
+        if(Gate::denies('noticia-update', $noticia)){
+            abort(403, "Não Autorizado!");
+        }
+        return view('noticia-update', compact('noticia'));
+    }
+
+
 }
