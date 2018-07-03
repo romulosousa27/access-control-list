620dba6dc47d9f83887fc133013cd3c53012fc15
Romulo Sousa <romulosousa27@hotmail.com>
2018-07-03 20:13:52 -0300

    Definindo Habilidades de Permissão Dinâmicamente

--- acl/app/Permission.php
+++ acl/app/Permission.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+
+    // retornando todas as regras(rules) das permissões(permission)
+    public function rules(){
+        return $this->belongsToMany(Rules::class);
+    }
 }
--- acl/app/Providers/AuthServiceProvider.php
+++ acl/app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\User;
 use App\Noticia;
+use App\Permission;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,22 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        //definindo autorização
+        /*
+            Gate::define('noticia-update', function(User $user, Noticia $noticia){
+                return $user->id == $noticia->user_id;
+            });
+         */
+
+        //Recuperando todas as permissões
+        $permissions = Permission::with('rules')->get();
+        foreach ($permissions as $permission){
+            Gate::define($permission->name , function(User $user) use ($permission){
+                return $user->hasPermission();
+            });
+        }
+
+
     }
 }
