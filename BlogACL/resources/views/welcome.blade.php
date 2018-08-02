e49031bb4c4386e3e2829ca2f4d958509d82b755
Romulo Sousa <romulosousa27@hotmail.com>
2018-08-01 21:18:11 -0300

    view welcome auth

--- BlogACL/resources/views/welcome.blade.php
+++ BlogACL/resources/views/welcome.blade.php
@@ -69,10 +69,10 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Painel</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('register') }}">Cadastra-se</a>
                     @endauth
                 </div>
             @endif
