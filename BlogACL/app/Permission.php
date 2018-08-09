b9825301db2df9d2db90f61897599942fba83d7c
Romulo Sousa <romulosousa27@hotmail.com>
2018-08-08 21:28:18 -0300

    relacionamento regras

--- BlogACL/app/Permission.php
+++ BlogACL/app/Permission.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    //todas as regras vinculada a permissão.
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 }
