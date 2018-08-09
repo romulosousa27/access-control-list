a43caf7e473ac6de1f32cd7f256053d55dd92900
Romulo Sousa <romulosousa27@hotmail.com>
2018-08-08 21:31:33 -0300

    view debug list

---
+++ BlogACL/resources/views/debug.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <ul class="list-group">
+        <li class="list-group-item bg-success text-uppercase text-white">Usuário: {{$user}}</li>
+        <!-- REGRAS -->
+        @foreach ( $roles as $role )
+            <li class="list-group-item">{{$role->label}}</li>
+        @endforeach
+    </ul>
+</div>
+
+@endsection
