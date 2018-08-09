437feeae8e8cd7ab29fb2e854bedb0e9ab11d219
Romulo Sousa <romulosousa27@hotmail.com>
2018-08-08 21:31:52 -0300

    arrumando o front end

--- BlogACL/resources/views/home.blade.php
+++ BlogACL/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header bg-primary text-white">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -22,11 +22,13 @@
 
     <!-- Listando todos os Posts -->
     @forelse($posts as $post)
-        <h1>{{$post->title}}</h1>
-        <p>{{$post->description}}</p>
-        <b>Autor: {{$post->user->name}}</b>
-        <a href="{{url("/post/$post->id/update")}}">editar</a>
-        <hr>
+        @can('view', $post)
+            <h1>{{$post->title}}</h1>
+            <p>{{$post->description}}</p>
+            <b>Autor: {{$post->user->name}}</b>
+            <a href="{{url("/post/$post->id/update")}}">editar</a>
+            <hr>
+        @endcan
     @empty
         <p>Nenhum Post Cadastrado</p>
     @endforelse
